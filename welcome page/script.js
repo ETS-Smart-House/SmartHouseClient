a529f7cf19453fa13e12d3ba9a2b2068d2e092b7
djluka <luka.djordjevic7017@gmail.com>
2022-02-17 21:42:54 +0100

    asda

--- welcome page/script.js
+++ welcome page/script.js
@@ -1,4 +1,3 @@
-alert(1);
 let section = document.getElementsByTagName("section")[0];
 let button = document.getElementsByTagName("button")[0];
 function mouseOver(){
@@ -6,5 +5,5 @@ function mouseOver(){
     section.style.transition= "all 1s";
 }
 function mouseLeave(){
-    section.style.backgroundColor = "#010258";
+    section.style.backgroundColor = "white";
 }
