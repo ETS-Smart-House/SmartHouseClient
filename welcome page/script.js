c64a85f713a87eb0df240891ecbee0d605dfd82b
djluka <luka.djordjevic7017@gmail.com>
2022-02-18 00:09:57 +0100

    aS

--- welcome page/script.js
+++ welcome page/script.js
@@ -1,9 +1,9 @@
 let section = document.getElementsByTagName("section")[0];
 let button = document.getElementsByTagName("button")[0];
 function mouseOver(){
-    section.style.backgroundColor = "#F9F9F9" ;
+    section.style.backgroundColor = "#076AE0" ;
     section.style.transition= "all 1s";
 }
 function mouseLeave(){
-    section.style.backgroundColor = "red";
+    section.style.backgroundColor = "#010228";
 }
