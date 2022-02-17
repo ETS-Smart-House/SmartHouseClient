da7e42edbf6dbbaea61f82979735bd76ce0dad93
djluka <luka.djordjevic7017@gmail.com>
2022-02-17 17:53:07 +0100

    Prva promena

---
+++ start page/script.js
@@ -0,0 +1,5 @@
+let body = document.getElementsByTagName("body")[0];
+let logo = document.getElementById("logo");
+function backGround(){
+    window.location.href = 'file:///C:/Users/Luka/Desktop/takmicenje/web%20app/welcome%20page/index.html';
+}
