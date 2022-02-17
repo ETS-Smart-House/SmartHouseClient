aa239ac4513255d40d695cd59f87e60d36f48eb9
djluka <luka.djordjevic7017@gmail.com>
2022-02-17 21:13:35 +0100

    test

--- welcome page/script.js
+++ welcome page/script.js
@@ -1,5 +1,5 @@
 let section = document.getElementsByTagName("section")[0];
 let button = document.getElementsByTagName("button")[0];
 let text = document.getElementsByTagName("p")[2-6];
-button.addEventListener('mouseover', () => section.style.backgroundColor = "#000000" ,section.style.transition= "all 1s" )
-button.addEventListener('mouseleave', () => section.style.backgroundColor = "#076AE0")
+button.addEventListener('mouseover', () => section.style.backgroundColor = "white" ,section.style.transition= "all 1s" )
+button.addEventListener('mouseleave', () => section.style.backgroundColor = "#010228")
