9a4adc4d6491bb703e3739f7ae0f9ed0ee4e7176
djluka <luka.djordjevic7017@gmail.com>
2022-02-17 18:50:19 +0100

    redirect
    start page > welcome page

--- start page/script.js
+++ start page/script.js
@@ -1,5 +1,5 @@
 let body = document.getElementsByTagName("body")[0];
 let logo = document.getElementById("logo");
 function backGround(){
-    window.location.href = 'welcome page/index.html';
+    window.location.href = 'https://djluka.github.io/SmartHouse/welcome%20page/index.html';
 }
