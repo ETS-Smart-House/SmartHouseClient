72f02d857571dcf66af4e243b24db3245b427c98
djluka <luka.djordjevic7017@gmail.com>
2022-02-17 21:59:05 +0100

    Update script.js

--- welcome page/script.js
+++ welcome page/script.js
@@ -1,7 +1,7 @@
 let section = document.getElementsByTagName("section")[0];
 let button = document.getElementsByTagName("button")[0];
 function mouseOver(){
-    section.style.backgroundColor = "red" ;
+    section.style.backgroundColor = "#F9F9F9" ;
     section.style.transition= "all 1s";
 }
 function mouseLeave(){
