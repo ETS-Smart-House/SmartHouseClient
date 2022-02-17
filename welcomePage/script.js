e48094f9882130dd314edff949dc20003cf2dc7a
djluka <luka.djordjevic7017@gmail.com>
2022-02-17 21:27:39 +0100

    as

--- welcomePage/script.js
+++ welcomePage/script.js
@@ -1,5 +1,10 @@
+alert(1);
 let section = document.getElementsByTagName("section")[0];
 let button = document.getElementsByTagName("button")[0];
-let text = document.getElementsByTagName("p")[2-6];
-button.addEventListener('mouseover', () => section.style.backgroundColor = "#000000" ,section.style.transition= "all 1s" )
-button.addEventListener('mouseleave', () => section.style.backgroundColor = "#010228")
+function mouseOver(){
+    section.style.backgroundColor = "black" ;
+    section.style.transition= "all 1s";
+}
+function mouseLeave(){
+    section.style.backgroundColor = "#010258";
+}
