e287ee0ecba0368623143bd4324c651439300506
djluka <luka.djordjevic7017@gmail.com>
2022-02-17 18:46:36 +0100

    redirect

--- start page/script.js
+++ start page/script.js
@@ -1,5 +1,5 @@
 let body = document.getElementsByTagName("body")[0];
 let logo = document.getElementById("logo");
 function backGround(){
-    window.location.href = 'file:///C:/Users/Luka/Desktop/takmicenje/SmartHouse/welcome%20page/index.html';
+    window.location.href = 'https://djluka.github.io/SmartHouse/welcome%20page/index.html';
 }
