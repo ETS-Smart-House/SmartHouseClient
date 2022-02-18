0424b12ca2f7b41f1c9a882410a048736c319ad2
djluka <luka.djordjevic7017@gmail.com>
2022-02-18 03:42:44 +0100

    asdf

--- welcome page/script.js
+++ welcome page/script.js
@@ -1,10 +1,15 @@
 let section = document.getElementsByTagName("section")[0];
 let button = document.getElementsByTagName("button")[0];
 function mouseOver(){
-    section.style.backgroundColor = "#0d1f2d" ;
-    button.style.border = "5px solid #2176ff" ;
-    section.style.transition= "all 1s";
+    section.style.backgroundColor = "#469ef7";
+    section.style.transition= "all 0.5s";
 }
 function mouseLeave(){
     section.style.backgroundColor = "#2176ff";
+}
+
+
+let typing1 = document.getElementsByClassName("typing1").style;
+if (typing1.width = "16"){
+    alert(1)
 }
