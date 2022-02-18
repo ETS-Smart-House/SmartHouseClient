c892d98504a6b01748b5cbc1629541946d12c5b7
djluka <luka.djordjevic7017@gmail.com>
2022-02-18 01:23:11 +0100

    asd

--- start page/script.js
+++ start page/script.js
@@ -1,3 +1,3 @@
 function redirect(){
-    window.location.href = 'https://djluka.github.io/SmartHouse/welcome%20page/index.html';
+    window.location.href = '/SmartHouse/welcome%20page/index.html';
 }
