4dd19b4d4c075fb23015e267e8b38923b73c4864
djluka <luka.djordjevic7017@gmail.com>
2022-02-18 02:24:00 +0100

    promena

--- welcome page/script.js
+++ welcome page/script.js
@@ -1,9 +1,10 @@
 let section = document.getElementsByTagName("section")[0];
 let button = document.getElementsByTagName("button")[0];
 function mouseOver(){
-    section.style.backgroundColor = "#076AE0" ;
+    section.style.backgroundColor = "#0d1f2d" ;
+    button.style.border = "5px solid #2176ff" ;
     section.style.transition= "all 1s";
 }
 function mouseLeave(){
-    section.style.backgroundColor = "#010228";
+    section.style.backgroundColor = "#2176ff";
 }
