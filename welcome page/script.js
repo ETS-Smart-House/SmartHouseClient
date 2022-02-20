51d8fc810d449ba8be0c12c2945ee98163032d23
djluka <luka.djordjevic7017@gmail.com>
2022-02-20 22:53:05 +0100

    Update script.js

--- welcome page/script.js
+++ welcome page/script.js
@@ -1,19 +1,15 @@
-/*
 let body = document.getElementsByTagName("body")[0];
 let section = document.getElementsByTagName("section")[0];
-let button = document.getElementsByTagName("button")[0];
+let button = document.getElementsByTagName("a")[0];
 let img = document.getElementById("banner");
 function mouseOver(){
     body.style.transition= "all 0.7s";
     section.style.transition= "all 0.7s";
     section.style.backgroundColor = "#1a73e8";
     button.style.backgroundColor = "#252839";
-    img.innerHTML="<img src=\"https://github.com/djluka/SmartHouse/blob/main/welcome%20page/assets/baner.png\">";
 }
 function mouseLeave(){
     section.style.backgroundColor = "#252839";
     button.style.backgroundColor = "#1a73e8";
-    img.innerHTML="<img src=\"https://github.com/djluka/SmartHouse/blob/main/welcome%20page/assets/baner1.png\">";
 
 }
-*/
