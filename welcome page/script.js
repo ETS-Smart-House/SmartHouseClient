c3fd1e53ad80de6947fd29a26264aa0735c496f3
djluka <luka.djordjevic7017@gmail.com>
2022-02-20 20:37:52 +0100

    asd

--- welcome page/script.js
+++ welcome page/script.js
@@ -1,10 +1,17 @@
 let body = document.getElementsByTagName("body")[0];
 let section = document.getElementsByTagName("section")[0];
 let button = document.getElementsByTagName("button")[0];
+let img = document.getElementById("banner");
 function mouseOver(){
-    body.style.transition= "all 0.2s";
-    body.style.backgroundColor = "#2176ff";
+    body.style.transition= "all 0.7s";
+    section.style.transition= "all 0.7s";
+    section.style.backgroundColor = "#1a73e8";
+    button.style.backgroundColor = "#252839";
+    img.innerHTML="<img src="assets/baner.png">";
 }
 function mouseLeave(){
-    body.style.backgroundColor = "#469ef7";
+    section.style.backgroundColor = "#252839";
+    button.style.backgroundColor = "#1a73e8";
+    img.innerHTML="<img src="assets/baner1.png">";
+
 }
