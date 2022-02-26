e2a499436839002b824f6a574b3c3d1cfdc38416
djluka <luka.djordjevic7017@gmail.com>
2022-02-26 01:48:19 +0100

    123

--- heating/script.js
+++ heating/script.js
@@ -1,42 +1,49 @@
 let up = document.getElementById("img1");
 let down = document.getElementById("img2");
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-    window.onclick = function(event) {  
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-};
-let button = document.getElementById("dugme");
-var clicks = 0;
-button.addEventListener('click', function(){
-    clicks += 1;
-    if(clicks%2 == 0){
-        up.style.visibility ="hidden"
-        down.style.visibility = "visible";
-    }
-    else{
-        up.style.visibility ="visible"
-        down.style.visibility = "hidden";
-    }
-});
 let pa = document.getElementById("area");
 let first = document.getElementById("first");
 let second = document.getElementById("second");
 let box = document.getElementById("dropdown");
-first.addEventListener("click" , function(){
+let button = document.getElementById("dugme");
+
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};
+var clicks = 0;
+button.addEventListener('click', function() {
+    clicks += 1;
+    if (clicks % 2 == 0) {
+        up.style.visibility = "hidden"
+        down.style.visibility = "visible";
+        button.style.borderRadius = "20px"
+
+    } else {
+        up.style.visibility = "visible"
+        down.style.visibility = "hidden";
+        button.style.borderRadius = "20px 20px 0px 0px"
+        second.style.borderRadius = "0px 0px 20px 20px"
+    }
+});
+
+
+first.addEventListener("click", function() {
     pa.innerHTML = "First";
-    dropdown.style.animation = "move 2s linear forwards"
 
 });
-second.addEventListener("click" , function(){
+second.addEventListener("click", function() {
     pa.innerHTML = "Second"
 })
