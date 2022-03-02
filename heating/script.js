6227d072359cf5350c5db3f7c8a6bcc9b7a150b1
djluka <luka.djordjevic7017@gmail.com>
2022-03-03 00:44:57 +0100

    asd

--- heating/script.js
+++ heating/script.js
@@ -5,47 +5,35 @@ let checkBox = document.getElementById("chB");
 
 function promena() {
     area.innerHTML = "Prvi";
-    //     if (area.innerHTML == "Prvi" || area.innerHTML == "Drugi") {
-    //         checkBox.style.opacity = "1";
-    //     }
-    // }
+}
 
-    function promena1() {
-        area.innerHTML = "Drugi";
-        // if (area.innerHTML == "Prvi" || area.innerHTML == "Drugi") {
-        //     checkBox.style.opacity = "1";
-        // }
+function promena1() {
+    area.innerHTML = "Drugi";
+}
+
+
+document.addEventListener("click", e => {
+    const isDropdownButton = e.target.matches("[data-dropdown-button]");
+    if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) {
+        up.style.visibility = "hidden";
+        down.style.visibility = "visible";
+        return
     }
 
+    let currentDropdown
+    if (isDropdownButton) {
+        currentDropdown = e.target.closest("[data-dropdown]");
+        currentDropdown.classList.toggle("active");
+    } else {
+        up.style.visibility = "hidden";
+        down.style.visibility = "visible";
+    }
 
-    document.addEventListener("click", e => {
-        const isDropdownButton = e.target.matches("[data-dropdown-button]");
-        if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) {
-            up.style.visibility = "hidden";
-            down.style.visibility = "visible";
-            checkBox.style.opacity = "0"
-
-            return
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        if (dropdown === currentDropdown) {
+            dropdown.classList.remove("active");
+            up.style.visibility = "visible";
+            down.style.visibility = "hidden";
         }
-
-        let currentDropdown
-        if (isDropdownButton) {
-            currentDropdown = e.target.closest("[data-dropdown]");
-            currentDropdown.classList.toggle("active");
-        } else {
-            up.style.visibility = "hidden";
-            down.style.visibility = "visible";
-            checkBox.style.opacity = "0"
-        }
-
-        document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
-            if (dropdown === currentDropdown) {
-                dropdown.classList.remove("active");
-                up.style.visibility = "visible";
-                down.style.visibility = "hidden";
-                checkBox.style.opacity = "1"
-
-
-            }
-        })
     })
+})
