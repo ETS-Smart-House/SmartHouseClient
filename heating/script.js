132f46979d38b03bca7906ef291be789d15e421b
djluka <luka.djordjevic7017@gmail.com>
2022-03-02 01:40:51 +0100

    a

--- heating/script.js
+++ heating/script.js
@@ -1,5 +1,16 @@
 let up = document.getElementById("up");
 let down = document.getElementById("down");
+let area = document.getElementById("area");
+
+function promena() {
+    area.innerHTML = "Prvi";
+}
+
+function promena1() {
+    area.innerHTML = "Drugi";
+}
+
+
 document.addEventListener("click", e => {
     const isDropdownButton = e.target.matches("[data-dropdown-button]");
     if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) {
@@ -7,7 +18,6 @@ document.addEventListener("click", e => {
         down.style.visibility = "visible";
         return
     }
-
 
     let currentDropdown
     if (isDropdownButton) {
@@ -21,6 +31,7 @@ document.addEventListener("click", e => {
     document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
         if (dropdown === currentDropdown) {
             dropdown.classList.remove("active");
+
             up.style.visibility = "visible";
             down.style.visibility = "hidden";
         }
