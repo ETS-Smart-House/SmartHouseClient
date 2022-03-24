ca3f7405b8a08d033bf41e8d7dfb6eab5950025d
Goran <gdjordjevic1701@gmail.com>
2022-03-24 21:01:24 +0100

    Fixes

--- js/stats.js
+++ js/stats.js
@@ -1,35 +1,93 @@
-const buttons = document.querySelectorAll("button");
+const navLinks = document.querySelectorAll('nav li a')
 
-function selectActiveRoom(e) {
-    let activeButton = document.querySelector('button.active')
-    if (activeButton) {
-        activeButton.classList.remove('active')
-
-    }
+function selectActiveNav(e) {
+    const activeLink = document.querySelector('nav li a.active')
+    if (activeLink) activeLink.classList.remove('active')
     e.target.classList.add('active')
+    const location = e.target.getAttribute('data-location')
 
-    let activeHolder = document.querySelector('.holder.active')
-    if (activeHolder) {
-        activeHolder.classList.remove('active')
-    }
-    const room = e.target.getAttribute('id');
+    const activePanel = document.querySelector('section.holder.active')
+    if (activePanel) activePanel.classList.remove('active')
 
-    activeHolder = document.querySelector(`.holder#${room}`)
-    activeHolder.classList.add('active')
+    const panel = document.querySelector(`section.holder#${location}`)
+    panel.classList.add('active')
 }
-buttons.forEach(btn => btn.addEventListener('click', selectActiveRoom))
 
-function activeButton(e) {
-    const button = e.target.getAttribute('id');
-    const button1 = document.querySelector('#indoor');
-    const button2 = document.querySelector('#outdoor');
-    console.log(button);
-    if (button == "indoor") {
-        button1.style.opacity = "1"
-        button2.style.opacity = "0.5"
-    } else {
-        button1.style.opacity = "0.5"
-        button2.style.opacity = "1"
+navLinks.forEach(navLink => navLink.addEventListener('click', selectActiveNav))
+
+/************************************
+ Load content from API
+ */
+
+const apiUrl = 'http://localhost:8012/measurements'
+
+window.addEventListener("DOMContentLoaded", async () => {
+
+    const location = 'indoor'
+
+    const response = await axios.get(apiUrl, {
+        params: {location}
+    })
+
+    const historyPanel = document.querySelector(`section.holder#${location} .split.istorija`)
+    const nowPanel = document.querySelector(`section.holder#${location} .split.trenutno`)
+
+    const today = moment().format('YYYY-MM-DD')
+
+    for (const item of response.data) {
+        if (item['date'] === today) {
+            const {itemMarkup, line} = generateIndoorNowItem(item['temperature'], item['humidity'])
+            nowPanel.append(itemMarkup)
+        } else {
+            const {itemMarkup, line} = generateIndoorHistoryItem(item['date'], item['temperature'], item['humidity'])
+            historyPanel.append(itemMarkup)
+            historyPanel.append(line)
+        }
     }
+})
+
+function generateIndoorHistoryItem(date, temperature, humidity) {
+    const itemMarkup = document.createElement('div')
+    itemMarkup.classList.add('h-box')
+    itemMarkup.innerHTML = `
+                <div class="h-text">
+                    <div class="txt-section">
+                        <p class="h-temperature">${temperature}°</p>
+                        <div class="h-humidity">
+                            <p class="h-humidity">vlažnost</p>
+                            <p>${humidity}%</p>
+                        </div>
+                    </div>
+                    <div class="hsx-line"></div>
+                    <div class="h-date">
+                        <p>${date}</p>
+                    </div>
+                </div>`
+
+    const line = document.createElement('div')
+    line.classList.add('hx-line')
+
+    return {itemMarkup, line}
 }
-buttons.forEach(btn => btn.addEventListener('click', activeButton))
+
+
+function generateIndoorNowItem(temperature, humidity) {
+    const itemMarkup = document.createElement('div')
+    itemMarkup.classList.add('box')
+
+    itemMarkup.innerHTML = `
+            <div class="text">
+                <div class="temperature">
+                    <p id="temp-indoor">${temperature}°</p>
+                </div>
+                <div class="humidity">
+                    <p>vlažnost</p>
+                    <p id="humidity-indoor">${humidity}%</p>
+                </div>
+            </div>`
+
+    const line = document.createElement('div')
+    line.classList.add('hx-line')
+
+    return {itemMarkup, line}
+}
